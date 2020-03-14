f2c9d93556c1c1b21396986408febbe4485c8cbd
Cigdem Esen <ekremnilufer2@gmail.com>
2020-03-14 15:56:22 -0400

    3

--- src/git_day2/NextClass.java
+++ src/git_day2/NextClass.java
@@ -3,8 +3,12 @@ package git_day2;
 public class NextClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+	System.out.println("Hello world");	
+		
+		
+		
 	}
 
 }
